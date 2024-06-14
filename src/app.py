a3984e80683abf61d5ec1bf42f6a5d4d154b1026
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-06-14 15:04:04 +0200

    create sql and route for detail page

--- src/app.py
+++ src/app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/agents', methods=['GET'])
 def agents_route():
-    agents = data_manager.get_agents()
+    agents = data_manager.get_all_agents()
     return render_template('agents.html', agents=agents)
 
 
 @app.route('/agents/<agent_name>', methods=['GET'])
 def agent_details_route(agent_name):
-    return f"hi {agent_name}"
+    return f"{data_manager.get_abilities_by_agent(agent_name)}"
 
 
 if __name__ == '__main__':
--- src/data_manager.py
+++ src/data_manager.py
@@ -6,7 +6,7 @@ def save_agent(cursor, name, description, agent_image, background_image, role):
     query = """
         INSERT INTO agent(name, description, agent_image, background_image, role)
         VALUES (%(name)s, %(description)s, %(agent_image)s, %(background_image)s, %(role)s)
-        RETURNING id;
+        RETURNING name;
         """
     cursor.execute(query, {
         'name': name,
@@ -19,23 +19,46 @@ def save_agent(cursor, name, description, agent_image, background_image, role):
 
 
 @connection_handler
-def save_ability(cursor, name, description, image, agent_id):
+def save_ability(cursor, name, description, image, agent_name):
     query = """
-        INSERT INTO ability(name, description, image, agent_id)
-        VALUES (%(name)s, %(description)s, %(image)s, %(agent_id)s)
+        INSERT INTO ability(name, description, image, agent_name)
+        VALUES (%(name)s, %(description)s, %(image)s, %(agent_name)s)
         """
     cursor.execute(query, {
         'name': name,
         'description': description,
         'image': image,
-        'agent_id': agent_id
+        'agent_name': agent_name
     })
 
 
 @connection_handler
-def get_agents(cursor):
+def get_all_agents(cursor):
     query = """ 
         SELECT * FROM agent
         """
     cursor.execute(query)
+    return cursor.fetchall()
+
+
+@connection_handler
+def get_one_agent(cursor, agent_name):
+    query = """
+        SELECT * FROM agent
+        WHERE agent_name = %(agent_name)s
+        """
+    cursor.execute(query, {
+        'agent_name': agent_name
+    })
+    return cursor.fetchall
+
+
+@connection_handler
+def get_abilities_by_agent(cursor, agent_name):
+    query = """
+        SELECT name, description, image
+        FROM ability
+        WHERE agent_name = %(agent_name)s
+        """
+    cursor.execute(query, {'agent_name': agent_name})
     return cursor.fetchall()
