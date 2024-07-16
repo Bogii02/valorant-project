1988ae799812543d9305610d18b70d3976bb9b3c
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-07-16 10:58:53 +0200

    create search with suggestions

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
-
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 import data_manager
 import populate_db
 
@@ -12,19 +11,18 @@ def agents_route():
     agent_search = request.args.get('agent-search')
 
     if agent_search is not None:
-        agent_search = agent_search.capitalize()
-        agent = data_manager.get_agent_by_name(agent_search)
+        agent = data_manager.get_agent_by_name(agent_search.capitalize())
         if agent:
             return redirect(url_for('agent_details_route', agent_name=agent_search))
         else:
-            return render_template('agents.html', agents=[])
+            return render_template('agents.html', agents=[], query=agent_search)
 
     agents = data_manager.get_all_agents()
     if not agents:
         populate_db.save_agents(populate_db.get_agents())
         return redirect(url_for('agents_route'))
 
-    return render_template('agents.html', agents=agents)
+    return render_template('agents.html', agents=agents, query='')
 
 
 @app.route('/agents/<agent_name>', methods=['GET'])
@@ -42,6 +40,13 @@ def weapons_route():
 @app.route('/maps', methods=['GET'])
 def maps_route():
     return 'maps'
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', '').lower()
+    results = [agent[1] for agent in data_manager.get_all_agents() if query in agent[1].lower()]
+    return jsonify(results)
 
 
 if __name__ == '__main__':
